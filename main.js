9ca116cba60a565d5ca3ad930b8fa4d684a207fa
biggieliten <filipisaksson00@gmail.com>
2023-11-02 14:20:40 +0100

    finished

--- main.js
+++ main.js
@@ -1,5 +1,11 @@
+//Här deklareras alla globala variabler för funktionerna. "API_URL" hämtar själva länken. De 4 andra targetar diverse element i HTML med "querySelector" eller "getElementById". Dessa variabler används senare i olika funktioner.
 const API_URL = "https://majazocom.github.io/Data/solaris.json";
+const closeModal = document.querySelector(".closeModal");
+const popupModal = document.getElementById("modal");
+const starContainer = document.querySelector(".stars");
+const planetDiv = document.querySelector(".planets");
 
+// Funktionen "CreatePlanets" hämtar api-datan från länken och gör om den till läsbar data för JS genom "response.json". Funktionen skapar även alla planeter som "divvar" och skickar dom vidare till ".planets" i HTML genom "planetDiv" eftersom dem är kopplade till varandra. Funktionen har även en try catch som skall fånga upp ifall något går fel när vi antingen skall fetcha datan från apin eller skapa planet divvarna.
 const createPlanets = async (apiURL) => {
   try {
     const response = await fetch(apiURL);
@@ -21,14 +27,19 @@ const createPlanets = async (apiURL) => {
 
     return data;
   } catch (error) {
-    console.error("Fel:", error);
+    console.error("Fault:", error);
   }
 };
-
 let data = createPlanets(API_URL);
 
-const starContainer = document.querySelector(".stars");
+// Nedan har vi en funktion som skall stänga ner själva modalen som kommer kommer upp när man klickar på en planet.
+closeModal.addEventListener("click", () => closePopupModal);
 
+function closePopupModal() {
+  popupModal.style.display = "none";
+}
+
+// Detta är en funktion som skapar stjärnorna med en for-loop. Antalet stjärnor är 400 och då loopas funktionen fram tills att vi har nått 400 st "star-divvar" (se "const star" och villkoren i loopen). Funktionen randomizar även positionerna för alla stjärnor genom "math.random".
 function stars() {
   for (let i = 0; i < 400; i++) {
     const star = document.createElement("div");
@@ -40,11 +51,10 @@ function stars() {
     starContainer.appendChild(star);
   }
 }
-
 stars();
 
+// Här är funktionen där vi plockar fram all info från apin samt hämtar elementen från HTML med "getElementById". Vi lägger till datan från apiet genom att ta ".textcontent" = "planet.property". Alltså har vi i funktionen createPlanets kört foorloopen " data.forEach((planet) ", där i har plockat hem alla planeter och där vi kallar objecten "planets". Där med tar vi te.x "planet.latinName" för att få fram alla specifika latinNames.
 function popUp(planet) {
-  const popupModal = document.getElementById("modal");
   document.getElementById("planetName").textContent = `${planet.name}`;
   document.getElementById("latinName").textContent = `${planet.latinName}`;
   document.getElementById("desc").textContent = ` ${planet.desc}`;
@@ -65,7 +75,3 @@ function popUp(planet) {
   popupModal.style.display = "block";
   popupModal.style.display = "flex";
 }
-
-const planetDiv = document.querySelector(".planets");
-
-const onClick = document.querySelectorAll(".planets div");
